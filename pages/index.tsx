c42d4c554d212ee2dbd661167fc5bfc97bb6c8a3
Daniel Berigoi <daniel@wingravity.com>
2021-02-16 23:07:09 +0200

    Cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,39 @@
-import Head from "next/head";
 import { useIntl } from "react-intl";
-export default function Home() {
+
+import ProfileListHeader from "components/ProfileListHeader";
+import ProfileListItem from "components/ProfileListItem";
+
+const DoctorsList = (props) => {
   const { formatMessage: f } = useIntl();
-
   return (
-    <div>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1>Welcome!</h1>
-        <h2>{f({ id: "hello" })}</h2>
-      </main>
-
-      <footer>{/* Silence is golden */}</footer>
+    <div className="flex flex-col">
+      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <ProfileListHeader />
+              <tbody className="bg-white divide-y divide-gray-200">
+                {props.users.map((user) => (
+                  <ProfileListItem key={user.id} user={user} />
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export const getServerSideProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await res.json();
+
+  return {
+    props: {
+      users,
+    },
+  };
+};
+
+export default DoctorsList;
